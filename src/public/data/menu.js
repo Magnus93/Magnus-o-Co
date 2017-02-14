9b0ce7cf5194daf271a055a2d791df9e6ee9e58a
Magnus <magnus.ja.gustafsson@gmail.com>
2017-02-14 14:08:53 +0100

    changed menu.json to menu.js

---
+++ src/public/data/menu.js
@@ -0,0 +1,36 @@
+var menuData = {
+  "food": [
+    {
+      "name": "XL Burger",
+      "img": "burger.png",
+      "price": 40
+    }
+  ],
+
+  "drinks": [
+    {
+      "category": "beerNWine",
+      "name": "Öl/Vin",
+      "img": "beerNWine.jpg",
+      "content": [
+          {
+            "name": "Beer",
+            "img": "beer.jpg",
+            "price": 40
+          }
+      ]
+    },
+    {
+      "category": "alcoholFree",
+      "name": "Alkofritt",
+      "img": "alcoholFree.jpg",
+      "content": [
+        {
+          "name": "Cola",
+          "img": "cola.jpg",
+          "price": 20
+        }
+      ]
+    }
+  ]
+}
